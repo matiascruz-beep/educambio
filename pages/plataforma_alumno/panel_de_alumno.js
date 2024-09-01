b0fc369fa3317ba3c9bbfe86446d6409cc693b91
= <alan-2990@live.com.ar>
2024-09-01 02:25:01 -0300

    Update panel_de_alumno.js
    correcciones

--- pages/plataforma_alumno/panel_de_alumno.js
+++ pages/plataforma_alumno/panel_de_alumno.js
@@ -3,15 +3,15 @@ import Navegation from "@/components/Platform/Navegation"
 
 export default function Platform () {
     return (
-    <main class="bg-cover bg-gradient-to-r from-[#003366] to-[#56AEFF] pb-[6.7rem]">
+        <main class="bg-cover bg-gradient-to-r from-[#003366] to-[#56AEFF] pb-[6.7rem]">
         
         <div>
-            <Navegation/>
+            <Navegation user="Juan"/>
         </div>
 
         <div>
         
-        <Welcome name="alan"/>
+        <Welcome name="Juan"/>
 
         </div>
         
