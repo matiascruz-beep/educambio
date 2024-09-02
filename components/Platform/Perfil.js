fc90242e8ec1f715e77ac79c110927df253e12e0
camilavelazquez <camilavelazquez000@gmail.com>
2024-09-02 15:33:16 -0200

    actualización del perfil

--- components/Platform/Perfil.js
+++ components/Platform/Perfil.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-export default function Perfil ({}) {
+export default function Perfil ({name, prop}) {
     return (
-        <div className="foto flex flex-col pr-3 rounded-[100px] bg-custom-focus w-[530px] h-[510px] justify-center items-center shadow-[20px_20px_20px_rgba(0,0,0,0.5)]">
-        <img className="rounded-full object-cover w-[300px] h-[300px]" src="/images/student1.jpg" alt="Estudiante" />
-        <p className="text-white font-poppins text-[20px] mt-5">Juan</p>
-        <p className="text-white font-poppins text-[15px] mt-3">Estudiante</p>
-        </div>
+        <div className="foto flex flex-col pr-3 ml-20 rounded-[100px] bg-custom-focus w-[400px] h-[380px] justify-center items-center shadow-[20px_20px_20px_rgba(0,0,0,0.5)]">
+        <img className="rounded-full object-cover w-[220px] h-[220px]" src="/images/student1.jpg" alt="Estudiante" />
+        <p className="text-white font-poppins text-[18px] md:text-[20px] mt-5">{name}</p>
+        <p className="text-white font-poppins text-[13px] md:text-[15px] mt-3">{prop}</p>
+</div>
+
     )
 }
--- pages/plataforma_alumno/perfil_alumno.js
+++ pages/plataforma_alumno/perfil_alumno.js
@@ -4,22 +4,23 @@ import Formulario_perfil from "@/components/Platform/Formulario_perfil"
 
 export default function Platform () {
     return (
-        <main class="bg-cover bg-gradient-to-r from-[#003366] to-[#56AEFF] pb-[6.7rem] h-screen w-full">
+        <main class="bg-cover bg-gradient-to-r from-[#003366] to-[#56AEFF]  h-screen w-full">
+        
         <div class="w-full">
             <Navegation user="Juan" />
         </div>
 
-        <div class="flex justify-center items-center mt-8">
-            
-            <div>
-            <Perfil />
-            </div>
-            
-            <div className="flex items-center justify-center ml-8">
-            <Formulario_perfil />
-            </div>
-
+        <div className="flex flex-col h-screen mt-[-80px] md:flex-row md:justify-center md:items-center">
+        
+        <div className="flex justify-center items-center mb-4 md:mb-0">
+        <Perfil name="Juan" prop="Estudiante" />
         </div>
+
+        <div className="flex justify-center items-center md:ml-8">
+        <Formulario_perfil />
+        </div>
+        </div>
+
         </main>
 
     )
