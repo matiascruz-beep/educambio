cb21feb5bdef2f3cabef085b4840ffbb2e7cf1ee
Ailen <ailenvera2021@gmail.com>
2024-09-02 13:55:00 -0300

    carrousel

--- pages/plataforma_alumno/clases_alumno.js
+++ pages/plataforma_alumno/clases_alumno.js
@@ -1,59 +1,227 @@
 import React from 'react'
+import Image from "next/image";
 
 
 export default function clases_alumno() {
   return (
-    <main>
-            
-                <form class="form items-center flex flex-col mt-5" id="crearClaseForm"/>
-                    <div class="flex">
-                        <div class="mr-3">
-                            <div class="flex flex-col mt-4">
-                                <label class="text-white" for="materia">Materia:</label>
-                                <input class="text-black w-[400px] h-[40px] rounded-[40px] pl-4 border-2 border-transparent focus:border-custom-blue2 focus:outline-none" type="text" id="materia" name="materia" placeholder="ej: matemática" required/>
-                            </div>
-                            <div class="flex flex-col mt-4">
-                                <label class="text-white" for="contenido">Contenido:</label>
-                                <input placeholder="ej: Función cuadrática" class="text-black w-[400px] h-[40px] rounded-[40px] pl-4 border-2 border-transparent focus:border-custom-blue2 focus:outline-none" type="text" id="contenido" name="contenido" required/>
-                            </div>
-                            <div class="flex flex-col mt-4">
-                                <label class="text-white" for="fecha">Fecha:</label>
-                                <input placeholder="dd/mm/aa" class="text-black w-[400px] h-[40px] rounded-[40px] pl-4 pr-4 border-2 border-transparent focus:border-custom-blue2 focus:outline-none" type="date" id="fecha" name="fecha" required/>
-                            </div>
-                        </div>
-                        <div>
-                            <div class="flex flex-col mt-4">
-                                <label class="text-white" for="horario">Horario:</label>
-                                <input class="text-black w-[400px] h-[40px] rounded-[40px] pl-5 pr-4 border-2 border-transparent focus:border-custom-blue2 focus:outline-none" type="time" id="horario" name="horario" required/>
-                            </div>
-                            <div class="flex flex-col mt-4">
-                                <label class="text-white" for="duracion">Duración:</label>
-                                <select class="text-black w-[400px] h-[40px] rounded-[40px] pl-4 pr-4 border-2 border-transparent focus:border-custom-blue2 focus:outline-none" type="text" id="duracion" name="duracion" required>
-                                    <option value="">Seleccione la duración </option>
-                                    <option value="1 hora">1 hora</option>
-                                    <option value="1.5 horas">1.5 horas</option>
-                                    <option value="2 horas">2 horas</option>
-                                    <option value="2.5 horas">2.5 horas</option>
-                                    <option value="3 horas">3 horas</option>
-                                    <option value="3.5 horas">3.5 horas</option>
-                                    <option value="4 horas">4 horas</option>
-                                    <option value="4.5 horas">4.5 horas</option>
-                                    <option value="5 horas">5 horas</option>
-                                </select>
-                            </div>
-                            <div class="flex flex-col mt-4">
-                            <label class="text-white" for="meet">Meet:</label>
-                            <input  placeholder="ej: https://meet.google.com/abc-def-fgh" class="text-black w-[400px] h-[40px] rounded-[40px] pl-4 border-2 border-transparent focus:border-custom-blue2 focus:outline-none" type="text" id="meet" name="meet" required/>
-                            </div>  
-                        </div> 
-                    </div>
-                    <div class="flex justify-center w-[200px] h-[40px] rounded-3xl bg-[#0082D6] mt-5">
-                        <button class="text-white w-[200px] h-[40px]" type="submit" id="crear">Crear Clase</button>
-                    </div> 
-                    
-                
-       
-     
+    <main className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
+      <div className="container">
+        <div className="swiper-container">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide p-4">
+              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                <p className="text-blue-500 text-center py-2 text-xl font-poppins">
+                  MATEMÁTICA
+                </p>
+                <Image
+                  className="w-full h-40 object-cover"
+                  src="../src/img/perfil-clase-2.jpg"
+                  alt="Imagen de la tarjeta"
+                />
+                <div className="p-4">
+                  <h2 className="text-xl mb-2 font-bold">Juan Soliz</h2>
+                  <p className="text-gray-700 mb-1">
+                    Fecha de inicio: Jueves 9/9/24
+                  </p>
+                  <p className="text-gray-700 mb-1">Horario: 15:00 hs a 16:15 hs</p>
+                  <p className="text-gray-700 mb-1">Tema: Función lineal</p>
+                  <p className="text-gray-700 mb-4">Cupos disponibles: 15</p>
+                  <div className="flex justify-center">
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded ">
+                      Inscribirme
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="swiper-slide p-4">
+              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                <p className="text-blue text-center py-2 text-xl font-poppins">
+                  MATEMÁTICA
+                </p>
+                <Image
+                  className="w-full h-40 object-cover"
+                  src="../src/img/perfil-clase-2.jpg"
+                  alt="Imagen de la tarjeta"
+                />
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-2">Juan Soliz</h2>
+                  <p className="text-gray-700 mb-1">
+                    Fecha de inicio: Jueves 9/9/24
+                  </p>
+                  <p className="text-gray-700 mb-1">Horario: 15:00 hs a 16:15 hs</p>
+                  <p className="text-gray-700 mb-1">Tema: Función lineal</p>
+                  <p className="text-gray-700 mb-4">Cupos disponibles: 15</p>
+                  <div className="flex justify-center">
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded">
+                      Inscribirme
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="swiper-slide p-4">
+              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                <p className="text-blue text-center py-2 text-xl font-poppins">
+                  MATEMÁTICA
+                </p>
+                <Image
+                  className="w-full h-40 object-cover"
+                  src="../src/img/perfil-clase-2.jpg"
+                  alt="Imagen de la tarjeta"
+                />
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-2">Juan Soliz</h2>
+                  <p className="text-gray-700 mb-1">
+                    Fecha de inicio: Jueves 9/9/24
+                  </p>
+                  <p className="text-gray-700 mb-1">Horario: 15:00 hs a 16:15 hs</p>
+                  <p className="text-gray-700 mb-1">Tema: Función lineal</p>
+                  <p className="text-gray-700 mb-4">Cupos disponibles: 15</p>
+                  <div className="flex justify-center">
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded">
+                      Inscribirme
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="swiper-slide p-4">
+              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                <p className="text-blue text-center py-2 text-xl font-poppins">
+                  MATEMÁTICA
+                </p>
+                <Image
+                  className="w-full h-40 object-cover"
+                  src="../src/img/perfil-clase-2.jpg"
+                  alt="Imagen de la tarjeta"
+                />
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-2">Juan Soliz</h2>
+                  <p className="text-gray-700 mb-1">
+                    Fecha de inicio: Jueves 9/9/24
+                  </p>
+                  <p className="text-gray-700 mb-1">Horario: 15:00 hs a 16:15 hs</p>
+                  <p className="text-gray-700 mb-1">Tema: Función lineal</p>
+                  <p className="text-gray-700 mb-4">Cupos disponibles: 15</p>
+                  <div className="flex justify-center">
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded">
+                      Inscribirme
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="swiper-slide p-4">
+              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                <p className="text-blue text-center py-2 text-xl font-poppins">
+                  MATEMÁTICA
+                </p>
+                <Image
+                  className="w-full h-40 object-cover"
+                  src="../src/img/perfil-clase-2.jpg"
+                  alt="Imagen de la tarjeta"
+                />
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-2">Juan Soliz</h2>
+                  <p className="text-gray-700 mb-1">
+                    Fecha de inicio: Jueves 9/9/24
+                  </p>
+                  <p className="text-gray-700 mb-1">Horario: 15:00 hs a 16:15 hs</p>
+                  <p className="text-gray-700 mb-1">Tema: Función lineal</p>
+                  <p className="text-gray-700 mb-4">Cupos disponibles: 15</p>
+                  <div className="flex justify-center">
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded">
+                      Inscribirme
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="swiper-slide p-4">
+              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                <p className="text-blue text-center py-2 text-xl font-poppins">
+                  MATEMÁTICA
+                </p>
+                <Image
+                  className="w-full h-40 object-cover"
+                  src="../src/img/perfil-clase-2.jpg"
+                  alt="Imagen de la tarjeta"
+                />
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-2">Juan Soliz</h2>
+                  <p className="text-gray-700 mb-1">
+                    Fecha de inicio: Jueves 9/9/24
+                  </p>
+                  <p className="text-gray-700 mb-1">Horario: 15:00 hs a 16:15 hs</p>
+                  <p className="text-gray-700 mb-1">Tema: Función lineal</p>
+                  <p className="text-gray-700 mb-4">Cupos disponibles: 15</p>
+                  <div className="flex justify-center">
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded">
+                      Inscribirme
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="swiper-slide p-4">
+              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                <p className="text-blue text-center py-2 text-xl font-poppins">
+                  MATEMÁTICA
+                </p>
+                <Image
+                  className="w-full h-40 object-cover"
+                  src="../src/img/perfil-clase-2.jpg"
+                  alt="Imagen de la tarjeta"
+                />
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-2">Juan Soliz</h2>
+                  <p className="text-gray-700 mb-1">
+                    Fecha de inicio: Jueves 9/9/24
+                  </p>
+                  <p className="text-gray-700 mb-1">Horario: 15:00 hs a 16:15 hs</p>
+                  <p className="text-gray-700 mb-1">Tema: Función lineal</p>
+                  <p className="text-gray-700 mb-4">Cupos disponibles: 15</p>
+                  <div className="flex justify-center">
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded">
+                      Inscribirme
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="swiper-slide p-4">
+              <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+                <p className="text-blue text-center py-2 text-xl font-poppins">
+                  MATEMÁTICA
+                </p>
+                <Image
+                  className="w-full h-40 object-cover"
+                  src="../src/img/perfil-clase-2.jpg"
+                  alt="Imagen de la tarjeta"
+                />
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-2">Juan Soliz</h2>
+                  <p className="text-gray-700 mb-1">
+                    Fecha de inicio: Jueves 9/9/24
+                  </p>
+                  <p className="text-gray-700 mb-1">Horario: 15:00 hs a 16:15 hs</p>
+                  <p className="text-gray-700 mb-1">Tema: Función lineal</p>
+                  <p className="text-gray-700 mb-4">Cupos disponibles: 15</p>
+                  <div className="flex justify-center">
+                    <button className="px-4 py-2 bg-blue-500 text-white rounded">
+                      Inscribirme
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="swiper-button-next"></div>
+          <div className="swiper-button-prev"></div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
