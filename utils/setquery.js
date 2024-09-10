b0f32b63d7cace8c1c47e785a264e9ad3042322e
matias <mati19984@hotmail.com>
2024-09-10 20:50:41 -0300

    Agrego carpeta utils que exporta funciones

---
+++ utils/setquery.js
@@ -0,0 +1,34 @@
+export const setQuery = (model, {params, body, query}) => {
+    const queryParams = query && [ 
+        {   
+            price:{ $gte: query?.minPrice },
+            price:{ $lte: query?.maxPrice }
+        },
+        {
+            "package.cost": {$gte: query?.minCost},
+            "package.cost": {$lte: query?.maxCost}
+        }
+    ]
+    const bodyParams = body && [
+        { "payment.currency": body?.currency },
+        { 
+            "package.days":{$gte:body?.days}, 
+            "package.nights":{$gte:body?.nights} 
+        },
+        { 
+            travelDate: {$lte: body?.maxDate}, 
+            travelDate: {$gte: body?.minDate} 
+        }
+    ]
+    const pathParams = params && [
+        { "location.country": params?.countryId },
+        { "location.city": params?.cityId },
+        { "lodging.pension.type": params?.pensionId }
+    ]
+    const reQuery = {
+        ...queryParams,
+        ...bodyParams,
+        ...pathParams    
+    }
+    return reQuery
+}
