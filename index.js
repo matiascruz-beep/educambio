512c735a4e9e9cceb68358c9546f76aea14994df
matias <mati19984@hotmail.com>
2024-09-19 21:02:39 -0300

    agrego configuracion para conectar el back y front

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 import express from "express";
 import morgan from "morgan";
 import { engine } from "express-handlebars"
+import path from 'path'
+import { fileURLToPath } from 'url';
+import next from 'next';
+// Obtener __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 import dbConn from "./node/config/db.js";
 import auth from "./node/routes/auth.js";
@@ -9,65 +15,45 @@ import dest from "./node/routes/destination.js";
 import post from "./node/routes/post.js";
 import usr from "./node/routes/user.js";
 
-//1/9
-import path from "path";
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-//------
-
 // Declaraciones
-const app = express(); // Servidor Web
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev }); // Instancia de Next.js
+const handle = app.getRequestHandler(); // Manejador de solicitudes de Next.js
+const nextApp = next({ dev });
+const app2 = express(); // Servidor Web
 const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 3000;
 const msg = `servidor iniciado en ${HOST}:${PORT}`
 // Dependencias de Desarrollo
-app.use(morgan('dev'))
+app2.use(morgan('dev'))
 // Decodificacion de parametros URL y BODY
-app.use(express.urlencoded({extended: false}))
+app2.use(express.urlencoded({extended: false}))
 // Conexion Base de Datos
 dbConn()
-// Motor de Plantillas
-const view = process.cwd()+"/views/";
-const config = {
-    partialsDir: view+"partials",
-    layoutsDir: view+"layouts",
-    defaultLayout: view+"index",
-    extname: "hbs"
-}
-app.engine("hbs", engine(config))
-app.set("view engine", "hbs")
+
 // Procesamiento de Rutas
-app.use("/", auth);
-app.use("/destinations", dest);
-app.use("/users", usr);
-app.use("/posts", post)
+app2.use("/", auth);
+app2.use("/destinations", dest);
+app2.use("/users", usr);
+app2.use("/posts", post)
 
-// Servir archivos estáticos desde la carpeta 'src/panel_de_profesor/assets'
-app.use('/assets', express.static(path.join(__dirname, 'src/panel_de_profesor/assets')));
 
-// Servir imágenes desde la carpeta 'src/panel_de_profesor/images'
-app.use('/images', express.static(path.join(__dirname, 'src/panel_de_profesor/images')));
+// Inicializa Next.js
+app.prepare().then(() => {
+    // Configura la ruta para el formulario de login
+    app2.get('/', (req, res) => {
+        return app.render(req, res, '/'); // Renderiza la página de inicio
+    });
+    //Este middleware permite que Next.js maneje todas las demás rutas
+    app2.all('*', (req, res) => {
+        return handle(req, res);
+    });
 
-// Configurar la carpeta 'src/panel_de_profesor' para servir archivos estáticos
-app.use('/panel_de_profesor', express.static(path.join(__dirname, 'src/panel_de_profesor')));
-
-// Servir la carpeta 'src' completa para archivos estáticos generales
-app.use(express.static(path.join(__dirname, 'src')));
-
-// Esta ruta debe ir al final para no interferir con las otras rutas
-app.get("*", express.static("./src/inicio_sesion"));
-
-// Ruta para servir el archivo HTML después del inicio de sesión
-app.get('/inicio', (req, res) => {
-    const filePath = path.join(__dirname, '.', 'src', 'panel_de_profesor', 'index.html');
-    res.sendFile(filePath);
+    app2.listen(PORT, () => {
+        console.log(`Servidor en el puerto ${PORT}`);
+    });
+   // Inicializacion del Servidor
+//app2.listen(PORT, HOST, console.log(msg))
 });
 
-app.get('/test-js', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/panel_de_profesor/app2.js'));
-});
-// Inicializacion del Servidor
-app.listen(PORT, HOST, console.log(msg))
 export default app;
