74aa5473bc57295c51e1f0c348c1629418e83375
camilavelazquez <camilavelazquez000@gmail.com>
2024-09-21 14:34:16 -0200

    actualizaciones responsive

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home() {
             </a>
           </div>
 
-          <div className="hidden md:flex">
+          <div className="hidden lg:flex">
             <ul className="flex list-none gap-4 md:gap-8">
               <li className="p-2 md:p-4 hover:text-gray-400">
                 <a
@@ -55,7 +55,7 @@ export default function Home() {
             </ul>
           </div>
 
-          <div className="hidden md:flex">
+          <div className="hidden lg:flex">
             <ul className="flex">
               <li className="flex justify-center items-center w-[120px] md:w-[150px] h-[40px] md:h-[50px] text-center bg-[#0063C7] text-white rounded-[43px] shadow-lg transition duration-300 hover:translate-y-0.5 hover:shadow-2xl mr-2">
                 <a
@@ -76,7 +76,7 @@ export default function Home() {
             </ul>
           </div>
 
-          <div className="md:hidden">
+          <div className="lg:hidden">
             <button
               onClick={() => {
                 const menu = document.getElementById('mobile-menu');
@@ -98,7 +98,7 @@ export default function Home() {
           </div>
         </div>
 
-        <div id="mobile-menu" className="hidden md:hidden">
+        <div id="mobile-menu" className="hidden lg:hidden">
           <ul className="flex flex-col items-center bg-gray-100 p-4">
             <li className="p-4 hover:text-gray-400">
               <a href="#inicio" className="no-underline text-inherit text-lg font-normal">
@@ -162,25 +162,25 @@ export default function Home() {
       </div>
 
 
-<div className="bg-[url('/images/fondoSQ.png')] bg-cover bg-no-repeat flex justify-center items-center h-screen" id="sabiasque">
-    <div className="p-6 sm:p-12 max-w-screen-2xl">
-        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-[#0082D6]">
+      <div className="bg-[url('/images/fondoSQ.png')] bg-cover bg-no-repeat flex justify-center items-center min-h-screen overflow-hidden" id="sabiasque">
+        <div className="p-6 sm:p-12 max-w-screen-2xl w-full">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-[#0082D6]">
             ¿Sabías qué?
-        </h1>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 pt-8">
-            <p className="text-white text-lg sm:text-xl md:text-2xl pr-0 lg:pr-40">
-                Todas nuestras clases son impartidas por <span className="font-bold text-[#0063C7]">profesores y profesores</span> en formación que se ofrecen como voluntarios. Ellos están comprometidos con la excelencia educativa y dedicados a <span className="font-bold text-[#0063C7]">brindar un apoyo personalizado y eficaz. </span>
+          </h1>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 pt-8">
+            <p className="text-white text-lg sm:text-xl md:text-2xl pr-0 lg:pr-20">
+              Todas nuestras clases son impartidas por <span className="font-bold text-[#0063C7]">profesores y profesores</span> en formación que se ofrecen como voluntarios. Ellos están comprometidos con la excelencia educativa y dedicados a <span className="font-bold text-[#0063C7]">brindar un apoyo personalizado y eficaz.</span>
             </p>
             <p className="text-white text-lg sm:text-xl md:text-2xl">
-                Muchos estudiantes enfrentan desafíos debido a la falta de pedagogía efectiva en sus entornos educativos, y falta de recursos economicos. Nuestra plataforma busca llenar ese vacío, proporcionando una <span className="font-bold">educación gratuita de calidad</span> que se enfoca en las necesidades individuales de cada estudiante.
+              Muchos estudiantes enfrentan desafíos debido a la falta de pedagogía efectiva en sus entornos educativos, y falta de recursos económicos. Nuestra plataforma busca llenar ese vacío, proporcionando una <span className="font-bold">educación gratuita de calidad</span> que se enfoca en las necesidades individuales de cada estudiante.
             </p>
-        </div>
-        <div className="pt-10 sm:pt-14">
+          </div>
+          <div className="pt-10 sm:pt-14">
             <h3 className="text-2xl sm:text-3xl text-white font-bold">
-                ¿Te gustaría unirte como voluntario?
+              ¿Te gustaría unirte como voluntario?
             </h3>
             <p className="text-white text-lg sm:text-2xl">
-                Si eres profesor recibido o en formación y <span className="font-bold">te gustaría unirte</span> a nuestra causa, <span className="font-bold">¡te invitamos a hacerlo!</span> Juntos, podemos hacer una diferencia significativa en la educación de nuestros jóvenes.
+              Si eres profesor recibido o en formación y <span className="font-bold">te gustaría unirte</span> a nuestra causa, <span className="font-bold">¡te invitamos a hacerlo!</span> Juntos, podemos hacer una diferencia significativa en la educación de nuestros jóvenes.
             </p>
             <div className="py-6">
               <ul>
@@ -191,67 +191,64 @@ export default function Home() {
                 </li>
               </ul>
             </div>
-        </div>
-    </div>
-</div>
-
-
-
-
-<div className="bg-[url('/images/FONDO.png')] bg-cover bg-no-repeat bg-center min-h-screen flex items-center justify-center bg-neutral-700" id="nosotros">
-
-<div className="container mx-auto p-8 mt-12">
-    <h2 className="text-4xl font-bold text-center mb-8 text-[#0082D6]">¿Quiénes Somos?</h2>
-
-    <div className="flex flex-wrap justify-center gap-8">
-
-    <Integrantes cardimage="/images/D.jpg" name="Diana Corregidor" prof="Full-Stack Developer" state="Estudiante" link="https://www.linkedin.com/in/daiana-corregidor-35049a227/"/>
-    <Integrantes cardimage="/images/M.jpg" name="Matias Cruz" prof="Full-Stack Developer" state="Estudiante" link=""/>
-    <Integrantes cardimage="/images/AL.jpg" name="Alan Wozniezka" prof="Full-Stack Developer" state="Estudiante" link=""/>
-    <Integrantes cardimage="/images/L.jpg" name="Lucas Felix" prof="Full-Stack Developer" state="Estudiante" link=""/>
-    <Integrantes cardimage="/images/C.png" name="Camila Velazquez" prof="Full-Stack Developer" state="Estudiante" link=""/>
-    <Integrantes cardimage="/images/AILEN.jpg" name="Ailen Vera" prof="Full-Stack Developer" state="Estudiante" link=""/>
-
-    </div>
-
-
-    <div className="text-center mt-16 text-white">
-        <p>
-            OFRECEMOS TUTORIAS GRATUITAS A ESTUDIANTES DE SECUNDARIA DE AREAS DESFAVORECIDAS,
-            APOYO ACADEMICO DE ALTA CALIDAD SIN INCURRIR EN COSTOS ADICIONALES. REDUCIMOS LAS DISPARIDADES
-            EDUCATIVAS BRINDANDO A TODOS LOS ESTUDIANTES LA OPORTUNIDAD DE ALCANZAR SU MAXIMO POTENCIAL
-            ACADEMICO.
-        </p>
-    </div>
-
-</div>
-
-</div>
-
-
-    <div className="bg-[#5EAFF9]/10 size-full md:size-auto pb-10">
-      <div className="md:container mx-auto flex justify-between pb-5">
-        <div className="mt-5">
-          <p className="text-xl font-semibold">
-            Aprende a tu ritmo con recursos multimedia, interactivos y profesores dedicados
-          </p>
-        </div>
-
-
-        <div className="p-3">
-          <button className="rounded-[43px] w-[160px] h-[55px] bg-[#0063C7] active:scale-75 transition duration-150 text-white uppercase font-bold">Reserva una clase</button>
+          </div>
         </div>
       </div>
 
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 md:gap-10">
 
-        <Cardindex title="Clases Sincronicas" cardImage="/images/card1.png" description="Retroalimentacion inmediata durante las clases para aclarar dudas o corregir errores, aprovechando las funciones de chat en vivo."/>
-        <Cardindex title="Videos Educativos" cardImage="/images/card2.png" description="Conceptos clave de tus materias utilizando ejemplos y graficos visuales para facilitar la comprensión"/> 
-        <Cardindex title="Material Didáctico" cardImage="/images/card3.png" description="Actividades practicas, material de lectura, infografias para que puedan profundizar en los temas y desarrollar habilidades de comprension lectora"/>
-       
+
+
+      <div className="bg-[url('/images/FONDO.png')] bg-cover bg-no-repeat bg-center min-h-screen flex items-center justify-center bg-neutral-700" id="nosotros">
+
+      <div className="container mx-auto p-8 mt-12">
+          <h2 className="text-4xl font-bold text-center mb-8 text-[#0082D6]">¿Quiénes Somos?</h2>
+
+          <div className="flex flex-wrap justify-center gap-8">
+
+          <Integrantes cardimage="/images/D.jpg" name="Diana Corregidor" prof="Full-Stack Developer" state="Estudiante" link="https://www.linkedin.com/in/daiana-corregidor-35049a227/"/>
+          <Integrantes cardimage="/images/M.jpg" name="Matias Cruz" prof="Full-Stack Developer" state="Estudiante" link=""/>
+          <Integrantes cardimage="/images/AL.jpg" name="Alan Wozniezka" prof="Full-Stack Developer" state="Estudiante" link=""/>
+          <Integrantes cardimage="/images/L.jpg" name="Lucas Felix" prof="Full-Stack Developer" state="Estudiante" link=""/>
+          <Integrantes cardimage="/images/C.png" name="Camila Velazquez" prof="Full-Stack Developer" state="Estudiante" link=""/>
+          <Integrantes cardimage="/images/AILEN.jpg" name="Ailen Vera" prof="Full-Stack Developer" state="Estudiante" link=""/>
+
+          </div>
+
+
+          <div className="text-center mt-16 text-white">
+              <p>
+                  OFRECEMOS TUTORIAS GRATUITAS A ESTUDIANTES DE SECUNDARIA DE AREAS DESFAVORECIDAS,
+                  APOYO ACADEMICO DE ALTA CALIDAD SIN INCURRIR EN COSTOS ADICIONALES. REDUCIMOS LAS DISPARIDADES
+                  EDUCATIVAS BRINDANDO A TODOS LOS ESTUDIANTES LA OPORTUNIDAD DE ALCANZAR SU MAXIMO POTENCIAL
+                  ACADEMICO.
+              </p>
+          </div>
+
       </div>
 
-    </div>
+      </div>
+
+
+      <div className="bg-[#5EAFF9]/10 size-full md:size-auto pb-10">
+        <div className="md:container mx-auto flex justify-between pb-5">
+          <div className="mt-5">
+            <p className="text-xl font-semibold">
+              Aprende a tu ritmo con recursos multimedia, interactivos y profesores dedicados
+            </p>
+          </div>
+          <div className="p-3">
+            <button className="rounded-[43px] w-[160px] h-[55px] bg-[#0063C7] active:scale-75 transition duration-150 text-white uppercase font-bold">
+              Reserva una clase
+            </button>
+          </div>
+        </div>
+
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 md:gap-10">
+          <Cardindex title="Clases Sincronicas" cardImage="/images/card1.png" description="Retroalimentacion inmediata durante las clases para aclarar dudas o corregir errores, aprovechando las funciones de chat en vivo." />
+          <Cardindex title="Videos Educativos" cardImage="/images/card2.png" description="Conceptos clave de tus materias utilizando ejemplos y graficos visuales para facilitar la comprensión" />
+          <Cardindex title="Material Didáctico" cardImage="/images/card3.png" description="Actividades practicas, material de lectura, infografias para que puedan profundizar en los temas y desarrollar habilidades de comprension lectora" />
+        </div>
+      </div>
 
 
     <Footer id="contacto"/>
