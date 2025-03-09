0478de0d51a9ff4793d552d8cc9e4390ff5ec091
Matias <matiasprofe2021@gmail.com>
2025-03-09 10:37:14 -0300

    agrego logica de UserProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,22 @@
 import "@/styles/globals.css";
-
 import { Poppins } from 'next/font/google'
+import { UserProvider } from '@/context/UserContext';
 
-const poppins = Poppins ({subsets: ['latin'], variable: '--font-poppins', weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']});
-
-
+const poppins = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+});
 
 export default function App({ Component, pageProps }) {
-  return (  
-  <main className={poppins.variable}>
-    <Component {...pageProps} />
-  </main> 
- )
+  return (
+    <UserProvider>
+      <main className={poppins.variable}>
+        <Component {...pageProps} />
+      </main>
+    </UserProvider>
+  )
 }
+
 
 
